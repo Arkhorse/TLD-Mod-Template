05fc5d028931ca14119dcb1dc99a278f77909da2
Arkhorse <theillusivemman@gmail.com>
2023-06-09 18:41:46 -0300

    Change logging to current MelonWiki usage

--- VisualStudio/Utilities/Logger.cs
+++ VisualStudio/Utilities/Logger.cs
@@ -2,10 +2,10 @@ namespace TEMPLATE
 {
     public class Logger
     {
-        internal static void Log(string message, params object[] parameters)            => MelonLogger.Msg($"{message}", parameters);
-        internal static void LogWarning(string message, params object[] parameters)     => MelonLogger.Warning($"{message}", parameters);
-        internal static void LogError(string message, params object[] parameters)       => MelonLogger.Error($"{message}", parameters);
-        internal static void LogSeperator(params object[] parameters)                   => MelonLogger.Msg("==============================================================================", parameters);
+        public static void Log(string message, params object[] parameters)              => Melon<TEMPLATE>.Logger.Msg($"{message}", parameters);
+        public static void LogWarning(string message, params object[] parameters)       => Melon<TEMPLATE>.Logger.Warning($"{message}", parameters);
+        public static void LogError(string message, params object[] parameters)         => Melon<TEMPLATE>.Logger.Error($"{message}", parameters);
+        public static void LogSeperator(params object[] parameters)                     => Melon<TEMPLATE>.Logger.Msg("==============================================================================", parameters);
         public static void LogStarter()                                                 => Melon<TEMPLATE>.Logger.Msg($"Mod loaded with v{BuildInfo.Version}");
     }
 }
