5b0cf8de41eaf168abcc4f1445f9039048d3e5f1
Arkhorse <theillusivemman@gmail.com>
2023-06-09 18:41:29 -0300

    add LogStarter()

--- VisualStudio/Utilities/Logger.cs
+++ VisualStudio/Utilities/Logger.cs
@@ -6,5 +6,6 @@ namespace TEMPLATE
         internal static void LogWarning(string message, params object[] parameters)     => MelonLogger.Warning($"{message}", parameters);
         internal static void LogError(string message, params object[] parameters)       => MelonLogger.Error($"{message}", parameters);
         internal static void LogSeperator(params object[] parameters)                   => MelonLogger.Msg("==============================================================================", parameters);
+        public static void LogStarter()                                                 => Melon<TEMPLATE>.Logger.Msg($"Mod loaded with v{BuildInfo.Version}");
     }
 }
