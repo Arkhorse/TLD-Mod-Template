779082febdb9b66602149298790ad3d406ec59dc
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-15 16:09:15 -0300

    add gearspawn stuff

--- VisualStudio/Settings/Settings.cs
+++ VisualStudio/Settings/Settings.cs
@@ -1,50 +1,62 @@
-using ModSettings;
-
 namespace TEMPLATE
 {
-    internal class Settings : JsonModSettings
+    public class Settings : JsonModSettings
     {
         internal static Settings Instance { get; } = new();
 
-        #region Settings
-        // Setup your settings here
-        [Section("Section")] // this is a section attrib. Anything inside of it will be collapsable within this section and will also be indented. Always in caps
-        [Name("Name")] // this is a name for a setting. Always in caps
-        [Description("Description")] // this is the description for the above setting. Shown when hovering on the right side
-        public bool name = true; // these should always be public and not static (otherwise you will need to move some code)
+        [Section("Spawn Rates")]
 
-        [Name("Mod Toggle")]
-        public bool EnableMod = true;
+        [Name("Pilgram")]
+        [Slider(0f, 100f, 101)]
+        public float pilgramSpawnExpectation = 80f;
 
-        #endregion
+        [Name("Voyager")]
+        [Slider(0f, 100f, 101)]
+        public float voyagerSpawnExpectation = 70f;
+
+        [Name("Stalker")]
+        [Slider(0f, 100f, 101)]
+        public float stalkerSpawnExpectation = 50f;
+
+        [Name("Interloper")]
+        [Slider(0f, 100f, 101)]
+        public float interloperSpawnExpectation = 20f;
+
+        [Name("Challenge")]
+        [Description("NOT SUPPORTED")]
+        [Slider(0f, 100f, 101)]
+        public float challengeSpawnExpectation = 100f;
+
+        [Name("Story")]
+        [Description("NOT SUPPORTED")]
+        [Slider(0f, 100f, 101)]
+        public float storySpawnExpectation = 90f;
 
         // this is used to set things when user clicks confirm. If you dont need this ability, dont include this method
         protected override void OnConfirm()
         {
             base.OnConfirm();
+
+            //CHANGEME
+            TEMPLATE.Spawner.SetProbability(Pilgram, Instance.pilgramSpawnExpectation);
+            TEMPLATE.Spawner.SetProbability(Voyager, Instance.voyagerSpawnExpectation);
+            TEMPLATE.Spawner.SetProbability(Stalker, Instance.stalkerSpawnExpectation);
+            TEMPLATE.Spawner.SetProbability(Interloper, Instance.interloperSpawnExpectation);
+            TEMPLATE.Spawner.SetProbability(Challenge, Instance.challengeSpawnExpectation);
+            TEMPLATE.Spawner.SetProbability(Story, Instance.storySpawnExpectation);
         }
 
         // this is called whenever there is a change. Ensure it contains the null bits that the base method has
         protected override void OnChange(FieldInfo field, object? oldValue, object? newValue)
         {
-            if ( field.Name == nameof(name) )
-            {
-                Instance.Refresh();
-            }
             base.OnChange(field, oldValue, newValue);
         }
 
-        // use this as a shortcut to enable/disable and fields on change
-        internal void Refresh()
-        {
-            SetFieldVisible(nameof(name), Instance.EnableMod);
-        }
-        
         // This is used to load the settings
         internal static void OnLoad()
         {
-            Instance.AddToModSettings($"{BuildInfo.GUIName}");
-            Instance.Refresh();
+            Instance.AddToModSettings(BuildInfo.GUIName);
+            Instance.RefreshGUI();
         }
     }
 }
