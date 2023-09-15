3572fa38d81dd7890ee96734eada7cbcfaf4bdbc
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-15 16:09:00 -0300

    Update TEMPLATE.cs

--- VisualStudio/TEMPLATE.cs
+++ VisualStudio/TEMPLATE.cs
@@ -1,15 +1,20 @@
 namespace TEMPLATE
 {
-    internal class TEMPLATE : MelonMod
+    public class Main : MelonMod
     {
-        // This is used to init the mod. If you have no settings or other dependent mods, this method is not needed
         public override void OnInitializeMelon()
         {
-            Settings.OnLoad(); // used to load settings, if you have it defined
-            // This is enclosed in a build conditional. This means if the build is a DEBUG build, this code will be part of the mod. Otherwise it will not
-#if DEBUG
-            Log($"Mod has loaded with version: {BuildInfo.Version}");
-#endif
+            // initial setup of the spawner values
+            Spawner spawner = new(
+                Settings.Instance.pilgramSpawnExpectation,
+                Settings.Instance.voyagerSpawnExpectation,
+                Settings.Instance.stalkerSpawnExpectation,
+                Settings.Instance.interloperSpawnExpectation,
+                Settings.Instance.challengeSpawnExpectation,
+                Settings.Instance.storySpawnExpectation
+            );
+            spawner.Add();
+            Settings.OnLoad();
         }
     }
 }
