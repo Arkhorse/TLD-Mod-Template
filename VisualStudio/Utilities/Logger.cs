954832ad0c7800f318acef579ed792f035f850d3
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-09-15 16:08:50 -0300

    Update Logger.cs

--- VisualStudio/Utilities/Logger.cs
+++ VisualStudio/Utilities/Logger.cs
@@ -2,10 +2,10 @@ namespace TEMPLATE
 {
     public class Logger
     {
-        public static void Log(string message, params object[] parameters)              => Melon<TEMPLATE>.Logger.Msg($"{message}", parameters);
-        public static void LogWarning(string message, params object[] parameters)       => Melon<TEMPLATE>.Logger.Warning($"{message}", parameters);
-        public static void LogError(string message, params object[] parameters)         => Melon<TEMPLATE>.Logger.Error($"{message}", parameters);
-        public static void LogSeperator(params object[] parameters)                     => Melon<TEMPLATE>.Logger.Msg("==============================================================================", parameters);
-        public static void LogStarter()                                                 => Melon<TEMPLATE>.Logger.Msg($"Mod loaded with v{BuildInfo.Version}");
+        public static void Log(string message, params object[] parameters)              => Melon<Main>.Logger.Msg($"{message}", parameters);
+        public static void LogWarning(string message, params object[] parameters)       => Melon<Main>.Logger.Warning($"{message}", parameters);
+        public static void LogError(string message, params object[] parameters)         => Melon<Main>.Logger.Error($"{message}", parameters);
+        public static void LogSeperator(params object[] parameters)                     => Melon<Main>.Logger.Msg("==============================================================================", parameters);
+        public static void LogStarter()                                                 => Melon<Main>.Logger.Msg($"Mod loaded with v{BuildInfo.Version}");
     }
 }
