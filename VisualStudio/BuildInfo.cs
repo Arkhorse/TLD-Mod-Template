9f8e19e85bd18a75bb16333a9c39e09abfa539c0
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2023-11-04 18:30:51 -0300

    Update BuildInfo.cs

--- VisualStudio/BuildInfo.cs
+++ VisualStudio/BuildInfo.cs
@@ -1,4 +1,14 @@
-﻿namespace TEMPLATE
+﻿// ---------------------------------------------
+// BuildInfo - by The Illusion
+// ---------------------------------------------
+// Reusage Rights ------------------------------
+// You are free to use this script or portions of it in your own mods, provided you give me credit in your description and maintain this section of comments in any released source code
+//
+// Warning !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// Ensure you change the namespace to whatever namespace your mod uses, so it doesnt conflict with other mods
+// ---------------------------------------------
+
+namespace TEMPLATE
 {
     public static class BuildInfo
     {
