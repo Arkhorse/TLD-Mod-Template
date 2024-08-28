eb44da5ddc4674090a463ce7d2b1520a0ac8352b
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-08-28 16:05:31 -0300

    Update JsonFile.cs

--- VisualStudio/Utilities/JSON/JsonFile.cs
+++ VisualStudio/Utilities/JSON/JsonFile.cs
@@ -8,114 +8,152 @@
 // Ensure you change the namespace to whatever namespace your mod uses, so it doesnt conflict with other mods
 // ---------------------------------------------
 using System.Text.Json;
-using TEMPLATE.Utilities;
-using TEMPLATE.Utilities.Exceptions;
+using System.Text.Json.Serialization;
 
 namespace TEMPLATE.Utilities.JSON
 {
-    public class JsonFile
-    {
-        /// <summary>
-        /// Gets the default set of options
-        /// </summary>
-        /// <returns></returns>
-        public static JsonSerializerOptions GetDefaultOptions()
-        {
-            return new JsonSerializerOptions()
-            {
-                WriteIndented = true,   // pretty print
-                IncludeFields = true    // use [JsonInclude] on properties you want to include, otherwise it wont be
-            };
-        }
+	/// <summary>
+	/// 
+	/// </summary>
+	public class JsonFile
+	{
+		#region Syncronous
+		/// <summary>
+		/// Save the JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="Tinput">An instance of <typeparamref name="T"/> with the data you want to save</param>
+		/// <exception cref="BadMemeException"></exception>
+		public static void Save<T>(string configFileName, T? Tinput)
+		{
+			Save<T>(configFileName, Tinput, JsonFileOptions.GetDefaultOptions());
+		}
+		/// <summary>
+		/// Save the JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="Tinput">An instance of <typeparamref name="T"/> with the data you want to save</param>
+		/// <param name="options">OPTIONAL: Options to use when saving the file. You MUST use the same options to load the file</param>
+		/// <exception cref="BadMemeException"></exception>
+		public static void Save<T>(string configFileName, T? Tinput, JsonSerializerOptions? options = null)
+		{
+			if (Tinput == null) throw new BadMemeException($"Save<T>()::Instance of {nameof(T)} is null");
+			try
+			{
+				options ??= JsonFileOptions.GetDefaultOptions();
+				using FileStream file = File.Open(configFileName, FileMode.Create, FileAccess.Write, FileShare.None);
+				JsonSerializer.Serialize<T>(file, Tinput, options);
+				file.Dispose();
+			}
+			catch (System.Exception e)
+			{
+				throw new BadMemeException($"Attempting to save {configFileName} failed", e);
+			}
+		}
 
-        #region Syncronous
-        /// <summary>
-        /// Save the JSON file
-        /// </summary>
-        /// <typeparam name="T">The class reference</typeparam>
-        /// <param name="configFileName">The absolute path of the file to save</param>
-        /// <param name="Tinput">An instance of <typeparamref name="T"/> with the data you want to save</param>
-        /// <param name="options">OPTIONAL: Options to use when saving the file. You MUST use the same options to load the file</param>
-        /// <exception cref="BadMemeException"></exception>
-        public static void Save<T>(string configFileName, T Tinput, JsonSerializerOptions? options = null)
-        {
-            try
-            {
-                options ??= GetDefaultOptions();
-                using FileStream file = File.Open(configFileName, FileMode.Create, FileAccess.Write, FileShare.None);
-                JsonSerializer.Serialize<T>(file, Tinput, options);
-                file.Dispose();
-            }
-            catch (System.Exception e)
-            {
-                throw new BadMemeException($"Attempting to save {configFileName} failed", e);
-            }
-        }
+		/// <summary>
+		/// Load a JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="createFile">Create the file if it doesnt exist</param>
+		/// <returns>An instance of <typeparamref name="T"/> with the data desearalized from the JSON file</returns>
+		/// <exception cref="BadMemeException"></exception>
+		public static T? Load<T>(string configFileName, bool createFile = false)
+		{
+			return Load<T>(configFileName, createFile, JsonFileOptions.GetDefaultOptions());
+		}
 
-        /// <summary>
-        /// Load a JSON file
-        /// </summary>
-        /// <typeparam name="T">The class reference</typeparam>
-        /// <param name="configFileName">The absolute path of the file to save</param>
-        /// <param name="createFile">Create the file if it doesnt exist</param>
-        /// <param name="options">OPTIONAL: Options to use when saving the file. You MUST use the same options to save the file</param>
-        /// <returns>An instance of <typeparamref name="T"/> with the data desearalized from the JSON file</returns>
-        /// <exception cref="BadMemeException"></exception>
-        public static T? Load<T>(string configFileName, bool createFile = false, JsonSerializerOptions? options = null)
-        {
-            if (!File.Exists(configFileName))
-            {
-                if (createFile)
-                {
-                    Save<T>(configFileName, default(T), options);
-                }
-                else
-                {
-                    throw new BadMemeException($"Requested JSON file does not exist, {configFileName}");
-                }
-            }
-            try
-            {
-                options ??= GetDefaultOptions();
-                using FileStream file = File.Open(configFileName, FileMode.Open, FileAccess.Read, FileShare.Read);
-                var output = JsonSerializer.Deserialize<T>(file, options);
-                file.Dispose();
-                return output;
-            }
-            catch (System.Exception e)
-            {
-                throw new BadMemeException($"Attempting to load the config file failed, file: {configFileName}", e);
-            }
-        }
-        #endregion
-        #region Async
+		/// <summary>
+		/// Load a JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="createFile">Create the file if it doesnt exist</param>
+		/// <param name="options">OPTIONAL: Options to use when saving the file. You MUST use the same options to save the file</param>
+		/// <returns>An instance of <typeparamref name="T"/> with the data desearalized from the JSON file</returns>
+		/// <exception cref="BadMemeException"></exception>
+		public static T? Load<T>(string configFileName, bool createFile = false, JsonSerializerOptions? options = null)
+		{
+			if (!File.Exists(configFileName))
+			{
+				if (createFile)
+				{
+					Save<T>(configFileName, default, options);
+				}
+				else
+				{
+					throw new BadMemeException($"JsonFile.Load({configFileName}, {createFile})::Requested JSON file does not exist");
+				}
+			}
+			try
+			{
+				options ??= JsonFileOptions.GetDefaultOptions();
+				using FileStream file = File.Open(configFileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+				var output = JsonSerializer.Deserialize<T>(file, options);
+				file.Dispose();
+				return output;
+			}
+			catch (System.Exception e)
+			{
+				throw new BadMemeException($"Attempting to load the config file failed, file: {configFileName}", e);
+			}
+		}
+		#endregion
+		#region Async
+		/// <summary>
+		/// Async load a JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="createFile">Create the file if it doesnt exist</param>
+		/// <returns>An instance of <typeparamref name="T"/> with the data desearalized from the JSON file</returns>
+		/// <exception cref="BadMemeException"></exception>
+		public static async Task<T?> LoadAsync<T>(string configFileName, bool createFile = false)
+		{
+			return await LoadAsync<T>(configFileName, createFile, JsonFileOptions.GetDefaultOptions());
+		}
+		/// <summary>
+		/// Async load a JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="createFile">Create the file if it doesnt exist</param>
+		/// <param name="options">OPTIONAL: Options to use when saving the file. You MUST use the same options to save the file</param>
+		/// <returns>An instance of <typeparamref name="T"/> with the data desearalized from the JSON file</returns>
+		/// <exception cref="BadMemeException"></exception>
+		public static async Task<T?> LoadAsync<T>(string configFileName, bool createFile = false, JsonSerializerOptions? options = null)
+		{
+			return await Task.Run(() => Load<T>(configFileName, createFile, options));
+		}
 
-        /// <summary>
-        /// Async load a JSON file
-        /// </summary>
-        /// <typeparam name="T">The class reference</typeparam>
-        /// <param name="configFileName">The absolute path of the file to save</param>
-        /// <param name="createFile">Create the file if it doesnt exist</param>
-        /// <param name="options">OPTIONAL: Options to use when saving the file. You MUST use the same options to save the file</param>
-        /// <returns>An instance of <typeparamref name="T"/> with the data desearalized from the JSON file</returns>
-        /// <exception cref="BadMemeException"></exception>
-        public static async Task<T?> LoadAsync<T>(string configFileName, bool createFile = false, JsonSerializerOptions? options = null)
-        {
-            return await Task.Run(() => Load<T>(configFileName, true, options));
-        }
+		/// <summary>
+		/// Async save the JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="Tinput">An instance of <typeparamref name="T"/> with the data you want to save</param>
+		/// <exception cref="BadMemeException"></exception>
+		public static async Task SaveAsync<T>(string configFileName, T? Tinput)
+		{
+			await SaveAsync<T>(configFileName, Tinput, JsonFileOptions.GetDefaultOptions());
+		}
 
-        /// <summary>
-        /// Async save the JSON file
-        /// </summary>
-        /// <typeparam name="T">The class reference</typeparam>
-        /// <param name="configFileName">The absolute path of the file to save</param>
-        /// <param name="Tinput">An instance of <typeparamref name="T"/> with the data you want to save</param>
-        /// <param name="options">OPTIONAL: Options to use when saving the file. You MUST use the same options to load the file</param>
-        /// <exception cref="BadMemeException"></exception>
-        public static async Task SaveAsync<T>(string configFileName, T Tinput, JsonSerializerOptions? options = null)
-        {
-            await Task.Run(() => Save<T>(configFileName, Tinput, options));
-        }
-        #endregion
-    }
+		/// <summary>
+		/// Async save the JSON file
+		/// </summary>
+		/// <typeparam name="T">The class reference</typeparam>
+		/// <param name="configFileName">The absolute path of the file to save</param>
+		/// <param name="Tinput">An instance of <typeparamref name="T"/> with the data you want to save</param>
+		/// <param name="options">OPTIONAL: Options to use when saving the file. You MUST use the same options to load the file</param>
+		/// <exception cref="BadMemeException"></exception>
+		public static async Task SaveAsync<T>(string configFileName, T? Tinput, JsonSerializerOptions? options = null)
+		{
+			if (Tinput == null) throw new BadMemeException($"SaveAsync<T>()::Instance of {nameof(T)} is null");
+			await Task.Run(() => Save<T>(configFileName, Tinput, options));
+		}
+		#endregion
+	}
 }
