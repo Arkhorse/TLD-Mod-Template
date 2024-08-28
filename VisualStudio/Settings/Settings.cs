35022af4fb2fdd732d0fc4dc823a8a676a7779d1
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-08-28 16:06:30 -0300

    Update Settings.cs

--- VisualStudio/Settings/Settings.cs
+++ VisualStudio/Settings/Settings.cs
@@ -1,62 +1,65 @@
 namespace TEMPLATE
 {
-    public class Settings : JsonModSettings
-    {
-        internal static Settings Instance { get; } = new();
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+	public class Settings : JsonModSettings
+	{
+		internal static Settings Instance { get; } = new();
 
-        [Section("Spawn Rates")]
+		#region  Spawner
+		[Section("Spawn Rates")]
 
-        [Name("Pilgram")]
-        [Slider(0f, 100f, 101)]
-        public float pilgramSpawnExpectation = 80f;
+		[Name("Pilgrim")]
+		[Slider(0f, 100f, 401)]
+		public float pilgrimSpawnExpectation = 80f;
 
-        [Name("Voyager")]
-        [Slider(0f, 100f, 101)]
-        public float voyagerSpawnExpectation = 70f;
+		[Name("Voyager")]
+		[Slider(0f, 100f, 401)]
+		public float voyagerSpawnExpectation = 70f;
 
-        [Name("Stalker")]
-        [Slider(0f, 100f, 101)]
-        public float stalkerSpawnExpectation = 50f;
+		[Name("Stalker")]
+		[Slider(0f, 100f, 401)]
+		public float stalkerSpawnExpectation = 50f;
 
-        [Name("Interloper")]
-        [Slider(0f, 100f, 101)]
-        public float interloperSpawnExpectation = 20f;
+		[Name("Interloper")]
+		[Slider(0f, 100f, 401)]
+		public float interloperSpawnExpectation = 20f;
 
-        [Name("Challenge")]
-        [Description("NOT SUPPORTED")]
-        [Slider(0f, 100f, 101)]
-        public float challengeSpawnExpectation = 100f;
+		[Name("Challenge")]
+		[Description("NOT SUPPORTED")]
+		[Slider(0f, 100f, 401)]
+		public float challengeSpawnExpectation = 100f;
 
-        [Name("Story")]
-        [Description("NOT SUPPORTED")]
-        [Slider(0f, 100f, 101)]
-        public float storySpawnExpectation = 90f;
+		[Name("Story")]
+		[Description("NOT SUPPORTED")]
+		[Slider(0f, 100f, 401)]
+		public float storySpawnExpectation = 90f;
+		#endregion
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 
-        // this is used to set things when user clicks confirm. If you dont need this ability, dont include this method
-        protected override void OnConfirm()
-        {
-            base.OnConfirm();
+		// this is used to set things when user clicks confirm. If you dont need this ability, dont include this method
+		/// <inheritdoc/>
+		protected override void OnConfirm()
+		{
+			base.OnConfirm();
+			// Delete this if your not using GearSpawner
+			Spawn.SetSpawner();
+		}
 
-            //CHANGEME
-            TEMPLATE.Spawner.SetProbability(Pilgram, Instance.pilgramSpawnExpectation);
-            TEMPLATE.Spawner.SetProbability(Voyager, Instance.voyagerSpawnExpectation);
-            TEMPLATE.Spawner.SetProbability(Stalker, Instance.stalkerSpawnExpectation);
-            TEMPLATE.Spawner.SetProbability(Interloper, Instance.interloperSpawnExpectation);
-            TEMPLATE.Spawner.SetProbability(Challenge, Instance.challengeSpawnExpectation);
-            TEMPLATE.Spawner.SetProbability(Story, Instance.storySpawnExpectation);
-        }
+		// this is called whenever there is a change. Ensure it contains the null bits that the base method has
+		/// <inheritdoc/>
+		protected override void OnChange(FieldInfo field, object? oldValue, object? newValue)
+		{
+			base.OnChange(field, oldValue, newValue);
+		}
 
-        // this is called whenever there is a change. Ensure it contains the null bits that the base method has
-        protected override void OnChange(FieldInfo field, object? oldValue, object? newValue)
-        {
-            base.OnChange(field, oldValue, newValue);
-        }
-
-        // This is used to load the settings
-        internal static void OnLoad()
-        {
-            Instance.AddToModSettings(BuildInfo.GUIName);
-            Instance.RefreshGUI();
-        }
-    }
+		// MUST be static
+		// This is used to load the settings
+		internal static void OnLoad()
+		{
+			Instance.AddToModSettings(BuildInfo.GUIName);
+			Instance.RefreshGUI();
+			// Delete this if your not using GearSpawner
+			Spawn.SetSpawner();
+		}
+	}
 }
