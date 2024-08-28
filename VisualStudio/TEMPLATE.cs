d5d2ca1fd39e7061ee936ccff1820c2122f7c2c0
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-08-28 16:06:35 -0300

    Update TEMPLATE.cs

--- VisualStudio/TEMPLATE.cs
+++ VisualStudio/TEMPLATE.cs
@@ -1,20 +1,67 @@
+#region System Directives
+global using System;
+global using System.Text;
+global using System.Text.RegularExpressions;
+#endregion
+#region Il2Cpp Directives
+global using Il2CppInterop.Runtime;
+global using Il2CppTLD.Gear;
+#endregion
+#region Unity Directives
+global using UnityEngine.AddressableAssets;
+#endregion
+#region Mod Directives
+global using TEMPLATE.Utilities;
+global using TEMPLATE.Utilities.Enums;
+global using TEMPLATE.Utilities.Exceptions;
+global using TEMPLATE.Utilities.JSON;
+global using ComplexLogger;
+#endregion
+
 namespace TEMPLATE
 {
-    public class Main : MelonMod
-    {
+
+    /// <inheritdoc/>
+	public class Main : MelonMod
+	{
+        /// <summary>
+        /// 
+        /// </summary>
+        public static ComplexLogger<Main> Logger = new();
+
+        /// <inheritdoc/>
         public override void OnInitializeMelon()
         {
-            // initial setup of the spawner values
-            Spawner spawner = new(
-                Settings.Instance.pilgramSpawnExpectation,
-                Settings.Instance.voyagerSpawnExpectation,
-                Settings.Instance.stalkerSpawnExpectation,
-                Settings.Instance.interloperSpawnExpectation,
-                Settings.Instance.challengeSpawnExpectation,
-                Settings.Instance.storySpawnExpectation
-            );
-            spawner.Add();
+            Spawn.SetupSpawner();
             Settings.OnLoad();
+        }
+
+        // used to do things when a scene is Initialized. This is what you want to use under normal circumstances
+        /// <inheritdoc/>
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasInitialized(buildIndex, sceneName);
+        }
+
+        // Triggered when the scene has finished loading
+        /// <inheritdoc/>
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasLoaded(buildIndex, sceneName);
+        }
+
+        // triggered when scenes are unloaded
+        /// <inheritdoc/>
+        public override void OnSceneWasUnloaded(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasUnloaded(buildIndex, sceneName);
+        }
+
+        // Use this to do things on update instead of patching GameManger.Update();
+        /// <inheritdoc/>
+        public override void OnLateUpdate()
+        {
+            base.OnLateUpdate();
         }
     }
 }
